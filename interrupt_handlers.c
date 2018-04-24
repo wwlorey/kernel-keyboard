8a93cf68b869029c7e07ecb77d2253ca85994824
William Lorey <wwlorey@gmail.com>
2018-04-23 22:22:05 -0500

    Fix index calculation, implement CAPSLOCK

--- interrupt_handlers.c
+++ interrupt_handlers.c
@@ -1,6 +1,9 @@
 #include "interrupt_handlers.h"
 #include "kbdus.h"
 
+#define TRUE	0x01
+#define FALSE	0x00
+
 char kbdus[128] =
 {
     0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
@@ -41,29 +44,39 @@ char kbdus[128] =
     0,	/* All other keys are undefined */
 };		
 
+
 __attribute__((interrupt))
 void keypress(void* frame)
 {
+  static unsigned char caps = FALSE;
+  static unsigned int caps_count = 0;
+  
   char buf[3];
   getScancodeByte(buf);
 
-  // TODO: I think this is wrong...
-  // If the scancode represents the END of a press, just make a newline
-  if (buf[0] & 0x8)
-    terminal_writestring("\n");
+  // Translate hex digits from buffer into integer index
+  int index = hex_to_int(buf);
+  
+  // Deal with a key release scancode
+  if (index & 0x80) {
+    if (caps_count >= 2 && caps && index == 186) { // The stopcode for caps has been seen, so disable it
+      caps = FALSE;
+      caps_count = 0;
+    }
+  }
   else {
-    // Translate hex digits from buffer into integer index
-    int index = 0;
-    index += (buf[0] - '0');
-    index = index << 4;
-    index += (buf[1] - '0');
+    // Determine if CAPSLOCK or shift is pressed
+    if (index == 58) { // CAPSLOCK
+      caps = TRUE;
+      caps_count += 1;
+    } 
 
     // Load the key value into a char array for writing to the screen
-    char key_val[2] = {kbdus[index]};
-
+    char key_val[2] = {get_char(index, caps)};
+  
     // Write the key value
     terminal_writestring(key_val);
-  }
+  } 
 
   // End the ISR (send end of interrupt)
   PIC_sendEOI(0x1);
@@ -73,4 +86,153 @@ __attribute__((interrupt))
 void hi(void* frame)
 {
   terminal_writestring("hi!\n");
+}
+
+int hex_to_int(const char* hex) {
+  unsigned int k;
+  unsigned int digit = 0;
+  int result = 0;
+
+  for (k = 0; k < 2; k++) {
+    if (hex[k] == '0')
+	digit = 0;
+    else if (hex[k] == '1') 
+	digit = 1;
+    else if (hex[k] == '2') 
+	digit = 2;
+    else if (hex[k] == '3') 
+	digit = 3;
+    else if (hex[k] == '4') 
+	digit = 4;
+    else if (hex[k] == '5') 
+	digit = 5;
+    else if (hex[k] == '6') 
+	digit = 6;
+    else if (hex[k] == '7') 
+	digit = 7;
+    else if (hex[k] == '8') 
+	digit = 8;
+    else if (hex[k] == '9') 
+	digit = 9;
+    else if (hex[k] == 'A') 
+	digit = 10;
+    else if (hex[k] == 'B') 
+	digit = 11;
+    else if (hex[k] == 'C') 
+	digit = 12;
+    else if (hex[k] == 'D') 
+	digit = 13;
+    else if (hex[k] == 'E') 
+	digit = 14;
+    else if (hex[k] == 'F') 
+	digit = 15;
+
+    result = result << 4;
+    result += digit;
+  }
+
+  return result;
+}
+
+char get_char(const int index, const unsigned char caps) {
+  char ret = kbdus[index];
+  
+  if (caps) {
+    if (ret == 'a')
+      return 'A';
+    else if (ret == 'b')
+      return 'B';
+    else if (ret == 'c')
+      return 'C';
+    else if (ret == 'd')
+      return 'D';
+    else if (ret == 'e')
+      return 'E';
+    else if (ret == 'f')
+      return 'F';
+    else if (ret == 'g')
+      return 'G';
+    else if (ret == 'h')
+      return 'H';
+    else if (ret == 'i')
+      return 'I';
+    else if (ret == 'j')
+      return 'J';
+    else if (ret == 'k')
+      return 'K';
+    else if (ret == 'l')
+      return 'L';
+    else if (ret == 'm')
+      return 'M';
+    else if (ret == 'n')
+      return 'N';
+    else if (ret == 'o')
+      return 'O';
+    else if (ret == 'p')
+      return 'P';
+    else if (ret == 'q')
+      return 'Q';
+    else if (ret == 'r')
+      return 'R';
+    else if (ret == 's')
+      return 'S';
+    else if (ret == 't')
+      return 'T';
+    else if (ret == 'u')
+      return 'U';
+    else if (ret == 'v')
+      return 'V';
+    else if (ret == 'w')
+      return 'W';
+    else if (ret == 'x')
+      return 'X';
+    else if (ret == 'y')
+      return 'Y';
+    else if (ret == 'z')
+      return 'Z';
+    else if (ret == '1')
+      return '!';
+    else if (ret == '2')
+      return '@';
+    else if (ret == '3')
+      return '#';
+    else if (ret == '4')
+      return '$';
+    else if (ret == '5')
+      return '%';
+    else if (ret == '6')
+      return '^';
+    else if (ret == '7')
+      return '&';
+    else if (ret == '8')
+      return '*';
+    else if (ret == '9')
+      return '(';
+    else if (ret == '0')
+      return ')';
+    else if (ret == '`')
+      return '~';
+    else if (ret == '-')
+      return '_';
+    else if (ret == '=')
+      return '+';
+    else if (ret == '[')
+      return '{';
+    else if (ret == ']')
+      return '}';
+    else if (ret == '\\')
+      return '|';
+    else if (ret == ';')
+      return ':';
+    else if (ret == '\'')
+      return '"';
+    else if (ret == ',')
+      return '<';
+    else if (ret == '.')
+      return '>';
+    else if (ret == '/')
+      return '?';
+  }
+  
+  return ret;
 }
--- interrupt_handlers.h
+++ interrupt_handlers.h
@@ -8,3 +8,11 @@ void keypress(void* frame);
 
 /* Print 'hi' to the screen */
 void hi(void* frame);
+
+/* Convert an array of hex chars to int */
+int hex_to_int(const char* hex);
+
+/* Return the charcter from kbdus at index. If caps is true, 
+ * return uppercase (or the character itself if uppercase
+ * is not valid). Otherwise return lowercase character. */
+char get_char(const int index, const unsigned char caps);
